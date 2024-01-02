63b9f1afd305e0b5a66c8c8c9f702365117c5c0d
KhoaSN <khoasinno@gmail.com>
2024-01-02 10:36:18 +0700

    init pj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context";
+
+// const HomePage = React.lazy(() => import("pages/HomePage"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="monkey-blogging-App">
+      <AuthProvider>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<></>}></Route>
+          </Routes>
+        </Suspense>
+      </AuthProvider>
     </div>
   );
 }
